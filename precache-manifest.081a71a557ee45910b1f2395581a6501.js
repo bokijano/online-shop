93a64edf8372fba5092a30693c2c4acf0a9ffd1b
Bojan Krstic <krstic.bojan80@gmail.com>
2019-04-30 18:52:39 +0200

    Updates

---
+++ precache-manifest.081a71a557ee45910b1f2395581a6501.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "52c47490a9ef1ed33e7a567e6bf1709d",
+    "url": "/online-shop/static/media/furniture9.52c47490.jpg"
+  },
+  {
+    "revision": "e3b28d4cad7f816360fe",
+    "url": "/online-shop/static/css/main.ab5d1c07.chunk.css"
+  },
+  {
+    "revision": "de22ddf6103542cfd170",
+    "url": "/online-shop/static/js/runtime~main.b83c20ff.js"
+  },
+  {
+    "revision": "aba529a34c990a523ba9",
+    "url": "/online-shop/static/js/2.b94617bb.chunk.js"
+  },
+  {
+    "revision": "d46c367a993cfe6703553b5e8aefc503",
+    "url": "/online-shop/static/media/furniture1.d46c367a.jpg"
+  },
+  {
+    "revision": "2311d27986983d4015b32cbd1e40f657",
+    "url": "/online-shop/static/media/furniture2.2311d279.jpg"
+  },
+  {
+    "revision": "e73d777ab0baa01dc8b6053f98ae5b2e",
+    "url": "/online-shop/static/media/furniture3.e73d777a.jpg"
+  },
+  {
+    "revision": "f4858197b28dd2225ac0876bb37cb201",
+    "url": "/online-shop/static/media/furniture4.f4858197.jpg"
+  },
+  {
+    "revision": "a16f259de468b08f921d71157a8dd032",
+    "url": "/online-shop/static/media/furniture5.a16f259d.jpg"
+  },
+  {
+    "revision": "35e900b1b5bdcb827158aa0e3b933ad9",
+    "url": "/online-shop/static/media/furniture6.35e900b1.jpg"
+  },
+  {
+    "revision": "cf30b02afffca949bbb0b4bb0bf49d22",
+    "url": "/online-shop/static/media/furniture7.cf30b02a.jpg"
+  },
+  {
+    "revision": "b8dafa985c3a23678083cfe5b62530dd",
+    "url": "/online-shop/static/media/furniture8.b8dafa98.jpg"
+  },
+  {
+    "revision": "e3b28d4cad7f816360fe",
+    "url": "/online-shop/static/js/main.7094f5c2.chunk.js"
+  },
+  {
+    "revision": "259653e815ea9aeaa13ddf7a0f78edea",
+    "url": "/online-shop/static/media/furniture10.259653e8.jpg"
+  },
+  {
+    "revision": "de3adcacfea0472ef8b262bb2a4fe36b",
+    "url": "/online-shop/static/media/furniture11.de3adcac.jpg"
+  },
+  {
+    "revision": "86d0e8b236da6b502e1cc082c44a3022",
+    "url": "/online-shop/static/media/furniture12.86d0e8b2.jpg"
+  },
+  {
+    "revision": "56e2494da505c5191179dda2a5634530",
+    "url": "/online-shop/static/media/back.56e2494d.jpg"
+  },
+  {
+    "revision": "bca539f76db9b0cd523e36e560e81dd3",
+    "url": "/online-shop/static/media/mob1.bca539f7.jpg"
+  },
+  {
+    "revision": "43af8f510d43eb593a9f1ede97576b08",
+    "url": "/online-shop/static/media/mob2.43af8f51.jpg"
+  },
+  {
+    "revision": "609d3c03eee780c3fde5dfc46792e1e3",
+    "url": "/online-shop/static/media/mob3.609d3c03.jpg"
+  },
+  {
+    "revision": "ba12301590cf594a610dc9e3c8134208",
+    "url": "/online-shop/static/media/mob4.ba123015.jpg"
+  },
+  {
+    "revision": "f40e67eb6c991d8a1c27eb79dd31fc94",
+    "url": "/online-shop/static/media/onlineShoping.f40e67eb.jpg"
+  },
+  {
+    "revision": "085333247a794766fe8ba232550eb55f",
+    "url": "/online-shop/static/media/furniture.08533324.jpg"
+  },
+  {
+    "revision": "f989b9a2a65dd366d619c5d9aee05c1f",
+    "url": "/online-shop/static/media/tv.f989b9a2.jpg"
+  },
+  {
+    "revision": "9443090bf8cbd3714f6e2c59a776b059",
+    "url": "/online-shop/index.html"
+  }
+];
